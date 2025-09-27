9404696ff261f263d395bcb3ba6976e5a5f4e367
Matheus Granville <matheus.granville@liferay.com>
2025-09-27 18:41:35 -0300

    Create Dashboard portlet

---
+++ modules/dashboard/dashboard-web/src/main/java/com/upray/dashboard/web/constants/DashboardPortletKeys.java
@@ -0,0 +1,11 @@
+package com.upray.dashboard.web.constants;
+
+/**
+ * @author me
+ */
+public class DashboardPortletKeys {
+
+	public static final String DASHBOARD =
+		"com_upray_dashboard_web_DashboardPortlet";
+
+}
---
+++ modules/dashboard/dashboard-web/src/main/java/com/upray/dashboard/web/portlet/DashboardPortlet.java
@@ -0,0 +1,29 @@
+package com.upray.dashboard.web.portlet;
+
+import com.upray.dashboard.web.constants.DashboardPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import jakarta.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author me
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"jakarta.portlet.display-name=Dashboard",
+		"jakarta.portlet.init-param.template-path=/",
+		"jakarta.portlet.init-param.view-template=/view.jsp",
+		"jakarta.portlet.name=" + DashboardPortletKeys.DASHBOARD,
+		"jakarta.portlet.resource-bundle=content.Language",
+		"jakarta.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class DashboardPortlet extends MVCPortlet {
+}
